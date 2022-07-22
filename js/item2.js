ace3031e22f6da4a90ed18319974152f84a62e1a
ousrahm <ousrahm1@gmail.com>
2022-07-22 00:01:00 -0400

    Added first and second page of item2

--- js/item2.js
+++ js/item2.js
@@ -1,3 +1,8 @@
-// $(document).ready(function () {
+$(document).ready(function () {
 
-// });
+    // When "Ready to Play!" button is pressed, open setup page.
+    $(document).on("click", "#ready-to-play", function() {
+        $("#main-game-container").empty().load("pages/game/setup.html");
+    })
+
+});
